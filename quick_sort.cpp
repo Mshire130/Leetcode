a7e0994c823280ddf6894cdba6b322125532f470
Mshire130 <44302575+Mshire130@users.noreply.github.com>
2021-06-11 23:33:11 +0100

    Update quick_sort.cpp
    fixed issues, pivot in start instead of middle

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -3,70 +3,38 @@
 
 using namespace std;
 
-
-int partition(vector<int> arr, int left, int right, int pivot){
-  int arr_left;
-  int arr_right;
-
-  while (left <= right){
-    while(arr[left] <= pivot){
-      left++;
-    }
-    while (arr[right] >= pivot){
-      right--;
-    }
-    
-    if(left <= right){
-      arr_left = arr[left];
-      arr_right = arr[right];
-
-      arr[left] = arr_right;
-      arr[right] = arr_left;
-
-      left++;
-      right--;
-    }
-
-  }
-
-
-    
-  // for(int i=0; i < arr.size(); i++){
-  //   cout << arr[i] << ", ";
-
-  // }
-  //   cout<< "\n";
-  return left;
+void swap(vector<int>* arr, int i, int j){
+  int temp = arr->at(i);
+  arr->at(i) = arr->at(j);
+  arr->at(j) = temp;
+  //arr[i] = arr[j];
+  //arr[j] = temp;
 }
 
+int partition(vector<int> &arr, int start, int end){
+  
+  int pivot = arr[start];
+  int swapIdx = start;
 
-//recursive section
-vector<int> quickSort(vector<int> arr, int left_idx, int right_idx){
-  //choosing pivot as middle of array
-  if (left_idx >= right_idx){
-    return arr;
+  for(int i = start + 1; i < arr.size(); i++){
+    if(pivot > arr[i]){
+      swapIdx++;
+      swap(&arr, swapIdx, i); 
+    }
   }
-  int size = arr.size();
-  int pivot = arr[(left_idx + right_idx)/2];
-  int index = partition(arr, left_idx, right_idx, pivot);
 
-  quickSort(arr, left_idx, index-1);
-  quickSort(arr, index, right_idx);
+  swap(&arr, start, swapIdx);
+  return swapIdx;
+}
 
+vector<int> quickSort(vector<int> &arr, int left, int right){
+  if(left < right){
+    int pivotIndex = partition(arr, left, right);
+    //Stuck here
+    cout << pivotIndex << ", " << left  << ", " << right ;
+    cout << endl;
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
   return arr;
 }
-
-
-int main(){
-  vector<int> arr = {12,13,5,2,5,100,15,3,2,1};
-  vector<int> sort_arr = quickSort(arr, 0, arr.size()-1);
-
-  for(int i=0; i < arr.size(); i++){
-    cout << sort_arr[i] << ", ";
-
-  }
-
-  return 0;
-}
-//helper function
-
