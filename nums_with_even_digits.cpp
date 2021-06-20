dad38ca07c1f3c78caa47e9aad6cba37d31a2dfd
Mshire130 <44302575+Mshire130@users.noreply.github.com>
2021-06-20 01:15:38 +0100

    Update and rename LFU.cpp to nums_with_even_digits.cpp

---
+++ nums_with_even_digits.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findNumbers(vector<int>& nums) {
+  int length = 0;
+  int evens = 0;
+
+  for(int i = 0; i< nums.size(); i++){
+    int element = nums[i];
+    while (element >= 10){
+      
+      cout<< element << "\n";
+      element = element / 10;
+      length++;
+    }
+    
+    if (length % 2 != 0){
+      evens++;
+    }
+
+    length =0;
+  }
+  
+  return evens;
+}
